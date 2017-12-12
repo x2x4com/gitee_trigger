2cc4d4dd46c0702b70a302a47ffc7fb945b24833
许向 <x2x4com@gmail.com>
2017-12-12 10:39:55 +0800

    first

---
+++ JsonFormat.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# ===============================================================================
+#
+#         FILE:
+#
+#        USAGE:
+#
+#  DESCRIPTION:
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  YOUR NAME (),
+#      COMPANY:
+#      VERSION:  1.0
+#      CREATED:
+#     REVISION:  ---
+# ===============================================================================
+
+from functools import wraps
+import json
+from flask import Response
+
+stand = {
+    "ret": 200,
+    "data": None,
+    "msg" : None
+}
+
+
+def output(format="json"):
+    def decorate(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            if type(result) == list:
+                try:
+                    stand['ret'] = int(result[0])
+                except IndexError:
+                    stand['ret'] = 500
+                    stand['data'] = None
+                    stand['msg'] = "return String error"
+                    return json.dumps(stand)
+                try:
+                    stand['data'] = result[1]
+                except IndexError:
+                    pass
+                try:
+                    stand['msg'] = result[2]
+                except IndexError:
+                    pass
+            else:
+                stand['data'] = result
+            result = json.dumps(stand)
+            return Response(result, mimetype='application/json')
+        return wrapper
+    return decorate
---
+++ UpdateHook.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# ===============================================================================
+#
+#         FILE:  
+#
+#        USAGE:    
+#
+#  DESCRIPTION:  
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  YOUR NAME (), 
+#      COMPANY:  
+#      VERSION:  1.0
+#      CREATED:  
+#     REVISION:  ---
+# ===============================================================================
+
+from flask import Flask, request
+from JsonFormat import output
+
+app = Flask(__name__)
+
+listen = '0.0.0.0'
+port = 10080
+processes = 4
+debug = False
+
+password_dict = [
+    'Ck-_DevzEAB6Yiy2',
+    'x2x4x2x4x2x4'
+]
+
+git_config = {
+    "name": "",
+    "root": ""
+}
+
+
+@app.route("/")
+@output(format="json")
+def index():
+    """
+    default router
+
+    :return:
+    """
+    return "/ => index"
+
+
+@app.route("/oschina/update.json", methods=['GET', 'POST'])
+@output(format="json")
+def update_json():
+    """
+    post data with json format
+
+    :return:
+    """
+    password = request.args.get('auth')
+    if request.method == 'POST' and password in password_dict:
+        content = request.get_json(force=True)
+
+    return "hello"
+
+
+@app.route("/oschina/update", methods=['GET','POST'])
+def update_form():
+    return "form"
+
+
+def do():
+    """
+
+    :return:
+    """
+    pass
+
+
+def check_git_dir(target):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(host=listen, port=port, processes=processes)
